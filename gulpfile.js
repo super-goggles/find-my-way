bbf6b637bf8832c54869a1305bdada0c3c7069fc
Adeola Adetoyese <adeolakautharadetoyese@mitt.ca>
2021-12-18 12:04:23 -0600

    create a build process that bundles my js and css files

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,29 @@
-const {src, dest, parallel, watch} = require('gulp');
+const { src, dest, parallel, watch } = require("gulp");
+const gulpWebpack = require("webpack-stream");
+const cleanCSS = require('gulp-clean-css');
 
-const processPages = () => {
-  return src('src/*.html').pipe(dest('dist'));
+function processPages() {
+  return src("src/*.html").pipe(dest("dist"));
 }
 
 const processStyles = () => {
-  return src('src/styles/*.css').pipe(dest('dist/styles'));
-}
+  return src("src/styles/*.css")
+  .pipe(cleanCSS())
+  .pipe(dest("dist/styles"));
+};
 
 const processScripts = () => {
-  return src('src/scripts/**/*.js').pipe(dest('dist/script'));
-}
+  return src("src/scripts/**/*.js")
+    .pipe(gulpWebpack({ mode: "production" }))
+    .pipe(dest("dist/scripts"));
+};
 
-exports.default = parallel(processPages, processStyles, processScripts)
+const watchTask = () => {
+  return watch(
+    ["src/**/**/**"],
+    parallel(processPages, processStyles, processScripts)
+  );
+};
+
+exports.default = parallel(processPages, processStyles, processScripts);
+exports.watch = watchTask;
