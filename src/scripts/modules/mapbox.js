5a761972d34caebcac286eb55f89d137663451e3
Adeola Adetoyese <adeolakautharadetoyese@mitt.ca>
2021-12-19 10:39:03 -0600

    enable selecting origins and destinations

--- src/scripts/modules/mapbox.js
+++ src/scripts/modules/mapbox.js
@@ -1,16 +1,10 @@
-let lat;
-let lon;
-
 let query = document.querySelectorAll("input");
-let originsList = document.getElementById("origins");
-let destinationsList = document.getElementById("destinations");
+let originsList = document.querySelector(".origins");
+let destinationsList = document.querySelector(".destinations");
 
 const getGeocode = (url) => {
   return fetch(url).then((response) => response.json());
 };
-
-query[0].addEventListener("keydown", handleOriginGeocode);
-query[1].addEventListener("keydown", handleDestinationGeocode);
 
 const renderDestinationsList = (geocodeDestinationsList) => {
   if (geocodeDestinationsList !== "") {
@@ -36,41 +30,54 @@ const renderOriginsList = (geocodeOriginsList) => {
   }
 };
 
-function handleOriginGeocode(e) {
+const handleOriginGeocode = (e) => {
   let origin = query[0].value;
   if (e.key === "Enter" && origin !== "") {
     originsList.innerHTML = "";
     e.preventDefault();
     getGeocode(
-      `https://api.mapbox.com/geocoding/v5/mapbox.places/${origin}.json?bbox=-97.325875,49.766204,-96.953987,49.99275&access_token=pk.eyJ1Ijoic3VwZXItZ29nZ2xlcyIsImEiOiJja3hiOTB4bTAwa2l1MnlxOWxjMTFjMmZ1In0.XsbEvgcsEukc_TyS67xL7w`
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${origin}.json?bbox=-97.325875,49.766204,-96.953987,49.99275&limit=10&access_token=pk.eyJ1Ijoic3VwZXItZ29nZ2xlcyIsImEiOiJja3hiOTB4bTAwa2l1MnlxOWxjMTFjMmZ1In0.XsbEvgcsEukc_TyS67xL7w`
     ).then((geocodeData) =>
       geocodeData.features.forEach((geocode) => {
         renderOriginsList(geocode);
       })
     );
   }
-}
+};
 
-function handleDestinationGeocode(e) {
+const handleDestinationGeocode = (e) => {
   let destination = query[1].value;
   if (e.key === "Enter" && destination !== "") {
     destinationsList.innerHTML = "";
     e.preventDefault();
     getGeocode(
-      `https://api.mapbox.com/geocoding/v5/mapbox.places/${destination}.json?bbox=-97.325875,49.766204,-96.953987,49.99275&access_token=pk.eyJ1Ijoic3VwZXItZ29nZ2xlcyIsImEiOiJja3hiOTB4bTAwa2l1MnlxOWxjMTFjMmZ1In0.XsbEvgcsEukc_TyS67xL7w`
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${destination}.json?bbox=-97.325875,49.766204,-96.953987,49.99275&limit=10&access_token=pk.eyJ1Ijoic3VwZXItZ29nZ2xlcyIsImEiOiJja3hiOTB4bTAwa2l1MnlxOWxjMTFjMmZ1In0.XsbEvgcsEukc_TyS67xL7w`
     ).then((geocodeData) =>
       geocodeData.features.forEach((geocode) => {
         renderDestinationsList(geocode);
       })
     );
   }
-}
+};
 
-// if (!navigator.geolocation) {
-//   console.log("Geolocation is not supported by your browser");
-// } else {
-//   navigator.geolocation.getCurrentPosition((position) => {
-//     lat = position.coords.latitude;
-//     lon = position.coords.longitude;
-//   });
-// }
+const handleSelectedLocation = (e) => {
+  const li = e.path.filter((el) => el.nodeName === "LI")[0];
+  li.classList.add("selected");
+};
+
+query[0].addEventListener("keydown", handleOriginGeocode);
+query[1].addEventListener("keydown", handleDestinationGeocode);
+originsList.addEventListener("click", (e) => {
+  let originList = document.querySelector(".origins").children;
+  for (let children of originList) {
+    children.classList.remove("selected");
+  }
+  handleSelectedLocation(e);
+});
+destinationsList.addEventListener("click", (e) => {
+  let destinationList = document.querySelector(".destinations").children;
+  for (let children of destinationList) {
+    children.classList.remove("selected");
+  }
+  handleSelectedLocation(e);
+});
